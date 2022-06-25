74b1134ca48c7258c5458be4eb41b4ab891d4de1
AntonioLinkinGitHub <108124441+AntonioLinkinGitHub@users.noreply.github.com>
2022-06-25 08:51:41 +0200

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+package com.open_bootcamp;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int resultado;
+        resultado = suma(29,66,12);
+        System.out.println(resultado);
+    }
+    public static int suma(int param1, int param2, int param3){
+
+        return param1 + param2 + param3;
+    }
+
+}
